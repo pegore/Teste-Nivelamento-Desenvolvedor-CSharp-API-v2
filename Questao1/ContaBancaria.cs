196012d3d2ea66548daa0bf52585d107a46265b2
Lino Pegoretti <lino.pegoretti@gmail.com>
2024-08-20 19:36:57 -0300

    q1 - Implementação da classe ContaBancaria e inserção de evidências

--- Questao1/ContaBancaria.cs
+++ Questao1/ContaBancaria.cs
@@ -2,8 +2,33 @@
 
 namespace Questao1
 {
-    class ContaBancaria {
+    class ContaBancaria
+    {
+        public int NumeroConta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
 
-       
+        public ContaBancaria(int numeroConta, string titular, double depositoInicial = 0.0)
+        {
+            NumeroConta = numeroConta;
+            Titular = titular;
+            Saldo = depositoInicial;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            const double taxa = 3.50;
+            Saldo -= (valor + taxa);
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {NumeroConta}, Titular: {Titular}, Saldo: $ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
     }
 }
